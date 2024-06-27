af1f838e468e707810dc16f325823f31c305c03f
Adrita Ghosh <167030199+AdritaGhosh@users.noreply.github.com>
2024-06-27 12:02:32 +0100

    Update Health Insurance.R

--- Health Insurance.R
+++ Health Insurance.R
@@ -8,15 +8,15 @@ str(dataset)
 range(dataset$age)
 range(dataset$bmi)
 range(dataset$children)
-range(dataset$charges)
+range(dataset$expenses)
 summary(dataset)
 sd(dataset$age)
 sd(dataset$bmi)
 sd(dataset$children)
-sd(dataset$charges)
+sd(dataset$expenses)
 shapiro.test(dataset$age)
 shapiro.test(dataset$bmi)
-shapiro.test(dataset$charges)
+shapiro.test(dataset$expenses)
 shapiro.test(dataset$children)
 
 # Visualization of distributions
@@ -31,9 +31,9 @@ ggplot(dataset, aes(x = age)) +
 ggplot(dataset, aes(x = bmi)) +
   geom_histogram(binwidth = 2, fill = "salmon", color = "black") +
   labs(title = "Distribution of BMI", x = "BMI", y = "Frequency")
-ggplot(dataset, aes(x = charges)) +
+ggplot(dataset, aes(x = expenses)) +
   geom_histogram(binwidth = 1000, fill = "lightgreen", color = "black") +
-  labs(title = "Distribution of Charges", x = "Charges", y = "Frequency")
+  labs(title = "Distribution of expenses", x = "expenses", y = "Frequency")
 
 # For the 'children' variable, a bar plot is used
 ggplot(dataset, aes(x = children)) +
@@ -58,27 +58,27 @@ ggplot(dataset, aes(y = age)) +
 ggplot(dataset, aes(y = bmi)) +
   geom_boxplot(fill = "salmon", color = "black") +
   labs(title = "Boxplot of BMI", y = "BMI")
-ggplot(dataset, aes(y = charges)) +
+ggplot(dataset, aes(y = expenses)) +
   geom_boxplot(fill = "lightgreen", color = "black") +
-  labs(title = "Boxplot of Charges", y = "Charges")
+  labs(title = "Boxplot of expenses", y = "expenses")
 
 install.packages("corrplot")
 library(corrplot)
 
 cor(dataset$age, dataset$bmi, method = "spearman")
 cor(dataset$age, dataset$children, method = "spearman")
-cor(dataset$age, dataset$charges, method = "spearman")
+cor(dataset$age, dataset$expenses, method = "spearman")
 cor(dataset$bmi, dataset$children, method = "spearman")
-cor(dataset$bmi, dataset$charges, method = "spearman")
-cor(dataset$children, dataset$charges, method = "spearman")
+cor(dataset$bmi, dataset$expenses, method = "spearman")
+cor(dataset$children, dataset$expenses, method = "spearman")
 
-cor(dataset[c("age", "bmi", "children", "charges")], method = "spearman")
-pairs(dataset[c("age", "bmi", "children", "charges")], method = "spearman")
+cor(dataset[c("age", "bmi", "children", "expenses")], method = "spearman")
+pairs(dataset[c("age", "bmi", "children", "expenses")], method = "spearman")
 
 
 install.packages('psych')
 library(psych)
-pairs.panels(dataset[c("age", "bmi", "children", "charges")], 
+pairs.panels(dataset[c("age", "bmi", "children", "expenses")], 
              method = "spearman", hist.col = "pink")
 
 #Chi-square test sex with smoker
@@ -97,65 +97,65 @@ print(chi_square_region_smoker)
 install.packages("tidyverse")
 library(tidyverse)
 
-model <- lm(charges ~ ., data = dataset)
+model <- lm(expenses ~ ., data = dataset)
 model
 summary(model)
 plot(model)
 model
 
-threshold <- median(dataset$charges)
-dataset$CHARGE_split <- ifelse(dataset$charges > threshold, "High", "Low")
+threshold <- median(dataset$expenses)
+dataset$EXPENSE_split <- ifelse(dataset$expenses > threshold, "High", "Low")
 
-"CHARGE_split" %in% names(dataset)
+"EXPENSE_split" %in% names(dataset)
 
-Utest_for_age <- wilcox.test(age ~ CHARGE_split, data = dataset)
+Utest_for_age <- wilcox.test(age ~ EXPENSE_split, data = dataset)
 Utest_for_age
-Utest_for_bmi <- wilcox.test(bmi ~ CHARGE_split, data = dataset)
+Utest_for_bmi <- wilcox.test(bmi ~ EXPENSE_split, data = dataset)
 Utest_for_bmi
-Utest_for_children <- wilcox.test(children ~ CHARGE_split, data = dataset)
+Utest_for_children <- wilcox.test(children ~ EXPENSE_split, data = dataset)
 Utest_for_children
 
-ggplot(dataset, aes(x = CHARGE_split, y = age, fill = CHARGE_split)) +
+ggplot(dataset, aes(x = EXPENSE_split, y = age, fill = EXPENSE_split)) +
   geom_boxplot() +
-  labs(x = "CHARGE_split", y = "Age") +
-  ggtitle("Comparison of Age Between CHARGE_split Groups")
+  labs(x = "EXPENSE_split", y = "Age") +
+  ggtitle("Comparison of Age Between EXPENSE_split Groups")
 
-ggplot(dataset, aes(x = CHARGE_split, y = bmi, fill = CHARGE_split)) +
+ggplot(dataset, aes(x = EXPENSE_split, y = bmi, fill = EXPENSE_split)) +
   geom_boxplot() +
-  labs(x = "CHARGE_split", y = "BMI") +
-  ggtitle("Comparison of BMI Between CHARGE_split Groups")
+  labs(x = "EXPENSE_split", y = "BMI") +
+  ggtitle("Comparison of BMI Between EXPENSE_split Groups")
 
-ggplot(dataset, aes(x = CHARGE_split, y = children, fill = CHARGE_split)) +
+ggplot(dataset, aes(x = EXPENSE_split, y = children, fill = EXPENSE_split)) +
   geom_boxplot() +
-  labs(title = "Comparison of children Between CHARGE_split Groups")
+  labs(title = "Comparison of children Between EXPENSE_split Groups")
 
 # Chi-square test for Sex
-sex_table <- table(dataset$sex, dataset$CHARGE_split)
+sex_table <- table(dataset$sex, dataset$EXPENSE_split)
 chi_square_sex <- chisq.test(sex_table)
 print(chi_square_sex)
 
 # Chi-square test for Smoker
-smoker_table <- table(dataset$smoker, dataset$CHARGE_split)
+smoker_table <- table(dataset$smoker, dataset$EXPENSE_split)
 chi_square_smoker <- chisq.test(smoker_table)
 print(chi_square_smoker)
 
 # Chi-square test for Region
-region_table <- table(dataset$region, dataset$CHARGE_split)
+region_table <- table(dataset$region, dataset$EXPENSE_split)
 chi_square_region <- chisq.test(region_table)
 print(chi_square_region)
 
 # Bar plot for categorical variables (e.g., Sex, Smoker, Region)
-ggplot(dataset, aes(x = sex, fill = CHARGE_split)) + 
+ggplot(dataset, aes(x = sex, fill = EXPENSE_split)) + 
   geom_bar(position = "dodge", stat = "count") +
-  labs(title = "Association of Insurance Charges by Sex", x = "Sex", y = "Count") +
+  labs(title = "Association of Insurance expenses by Sex", x = "Sex", y = "Count") +
   scale_fill_manual(values = c("Low" = "grey", "High" = "brown"))
-ggplot(dataset, aes(x = smoker, fill = CHARGE_split)) + 
+ggplot(dataset, aes(x = smoker, fill = EXPENSE_split)) + 
   geom_bar(position = "dodge", stat = "count") +
-  labs(title = "Association of Insurance Charges by Smoker", x = "Sex", y = "Count") +
+  labs(title = "Association of Insurance expenses by Smoker", x = "Sex", y = "Count") +
   scale_fill_manual(values = c("Low" = "grey", "High" = "brown"))
-ggplot(dataset, aes(x = region, fill = CHARGE_split)) + 
+ggplot(dataset, aes(x = region, fill = EXPENSE_split)) + 
   geom_bar(position = "dodge", stat = "count") +
-  labs(title = "Association of Insurance Charges by Region", x = "Sex", y = "Count") +
+  labs(title = "Association of Insurance expenses by Region", x = "Sex", y = "Count") +
   scale_fill_manual(values = c("Low" = "grey", "High" = "brown"))
 
 install.packages("readr")
@@ -169,9 +169,9 @@ library(dplyr)
 library(rstatix)
 library(PMCMRplus)
 
-kruskal_test_result_charges_region <- dataset %>%
-  kruskal_test(charges ~ region)
-kruskal_test_result_charges_region
+kruskal_test_result_expenses_region <- dataset %>%
+  kruskal_test(expenses ~ region)
+kruskal_test_result_expenses_region
 
 kruskal_test_result_age_region <- dataset %>% 
   kruskal_test(age ~ region)
